ef2e76780674d1a2c70404fbad78e17e1e769e43
SamiTekie <samitekie84@gmail.com>
2023-05-16 20:47:00 +0300

    Create Interfaces for Pet Clinic Services #5

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
@@ -2,15 +2,11 @@ package guru.springframework.sfgpetclinic.services;
 
 import guru.springframework.sfgpetclinic.model.Owner;
 
-import java.util.List;
+import java.util.Set;
 
-
-/**
- * Created by jt on 7/18/18.
- */
-public interface OwnerService extends CrudService<Owner, Long> {
-
+public interface OwnerService {
     Owner findByLastName(String lastName);
+    Owner findById(Long id);
+    Set<Owner> findAll();
 
-    List<Owner> findAllByLastNameLike(String lastName);
- }
+}
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetService.java
@@ -1,10 +1,12 @@
 package guru.springframework.sfgpetclinic.services;
 
+import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.model.Pet;
 
-/**
- * Created by jt on 7/18/18.
- */
-public interface PetService extends CrudService<Pet, Long> {
+import java.util.Set;
 
+public interface PetService {
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
 }
