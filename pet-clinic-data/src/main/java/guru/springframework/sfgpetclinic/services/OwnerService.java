e8fda20fda18dfea3c2f2d8d35ff6e7359dee691
SamiTekie <samitekie84@gmail.com>
2023-05-18 16:36:27 +0300

    Refactor to Common Crud Interface.Closes #11

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
@@ -2,11 +2,9 @@ package guru.springframework.sfgpetclinic.services;
 
 import guru.springframework.sfgpetclinic.model.Owner;
 
-import java.util.Set;
 
-public interface OwnerService {
+public interface OwnerService extends CrudeService<Owner,Long> {
     Owner findByLastName(String lastName);
-    Owner findById(Long id);
-    Set<Owner> findAll();
+
 
 }
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetService.java
@@ -3,10 +3,8 @@ package guru.springframework.sfgpetclinic.services;
 import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.model.Pet;
 
-import java.util.Set;
 
-public interface PetService {
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
+
+public interface PetService extends CrudeService<Pet,Long>{
+
 }
