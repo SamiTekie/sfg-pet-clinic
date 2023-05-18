8349512c7d9a8bdb351f9ca4acb4635e2a0c1502
SamiTekie <samitekie84@gmail.com>
2023-05-18 18:23:06 +0300

    Impl Map Based Services. Closes #6

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/CrudeService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/CrudeService.java
@@ -3,7 +3,7 @@ import java.util.Set;
 
 public interface CrudeService <T,ID> {
     Set<T> findAll();
-    T findById();
+    T findById(ID id);
 
     T save(T object);
     void deleteById(ID id);
